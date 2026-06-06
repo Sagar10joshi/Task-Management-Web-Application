33459715ffce3ff236c50be8eee0c20dbd68b972
Sagar Joshi <coderjoshi15@gmail.com>
2026-06-06 13:05:27 +0530

    frontend URL  changed

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://task-management-web-application-seven.vercel.app/"
+      "https://task-management-web-application-seven.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
